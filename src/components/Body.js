e9fefc5c8ca9d53fb6797c9ca414360f54cb2f90
s4sachin <sachinpurohit.text@gmail.com>
2024-05-21 07:20:57 +0530

    Introducing React State variables- Re-render, Reconciliation, Maintain data state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
-import {resList} from '../utils/constants'
+import { resList } from "../utils/constants";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+
   return (
     <div className="body">
-      <div className="search-bar">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setListOfRestaurants(
+              listOfRestaurants.filter((res) => res?.info?.avgRating >= 4)
+            );
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurant-container">
-        {resList.map((res) => {
+        {listOfRestaurants.map((res) => {
           return <RestaurantCard resData={res} key={res.info.id} />;
         })}
-        {/* <RestaurantCard resData={resList[0]} />
-          <RestaurantCard resData={resList[1]} />
-          <RestaurantCard resData={resList[2]} />
-          <RestaurantCard resData={resList[3]} />
-          <RestaurantCard resData={resList[4]} />
-          <RestaurantCard resData={resList[5]} />
-          <RestaurantCard resData={resList[6]} />
-          <RestaurantCard resData={resList[7]} /> */}
       </div>
     </div>
   );
