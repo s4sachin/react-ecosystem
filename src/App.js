82b525135de5d974d287c8ef7af5b4c410975c65
s4sachin <sachinpurohit.text@gmail.com>
2024-05-22 18:02:17 +0530

    Added Routing to the Application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 const AppLayout = () => {
   return (
-    <div className="app">
+    <React.Fragment>
       <Header />
-      <Body />
-    </div>
-  );
+      <Outlet />
+    </React.Fragment>
+  ); 
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />, 
+      }
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -10,10 +11,10 @@ export const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Cart</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/">Cart</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <button
             className="login-btn"
             onClick={() => {
